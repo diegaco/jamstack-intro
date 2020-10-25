b5c0da620c3770883c6c34fc74c52644dfe1c455
Diego Acosta <diegoacosta211@gmail.com>
2020-10-25 20:35:09 -0300

    feat(todos): delete todo

--- sections/todos/src/components/Todo.js
+++ sections/todos/src/components/Todo.js
@@ -12,15 +12,29 @@ const Todo = ({ todo, reloadTodos }) => {
     }).then(reloadTodos)
   };
 
+  const handleDelete = () => {
+    axios.post('/api/delete-todo', {
+      id: todo._id
+    }).then(reloadTodos);
+  }
+
   return (
     <>
       <label htmlFor={`todo-toggle-${ todo._id }`} className={styles.label}>
         Mark complete
       </label>
-      <input type="checkbox" id={`todo-toggle-${todo._id}`} checked={todo.completed} className={styles.toggle} onChange={toggleCompleted} />
+      <input type='checkbox' id={`todo-toggle-${todo._id}`} checked={todo.completed} className={styles.toggle} onChange={toggleCompleted} />
       <p className={`${styles.text} ${todo.completed && styles.completed}`}>
         {todo.text}
       </p>
+      <label htmlFor={`todo-delete-${todo._id}`} className={styles.label}>
+        Delete
+      </label>
+      <button onClick={handleDelete} className={styles.delete}>
+        <span role='img' aria-label='delete' title='delete this todo'>
+          ❌
+        </span>
+      </button>
     </>
   )
 }
